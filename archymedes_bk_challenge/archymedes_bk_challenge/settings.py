2f04087a1f7e8cae2767000d9be26b305cc6cfa6
Mapopo (Evans) <mppdgreat@gmail.com>
2022-02-04 01:54:42 +0100

    Set CORS headers

--- archymedes_bk_challenge/archymedes_bk_challenge/settings.py
+++ archymedes_bk_challenge/archymedes_bk_challenge/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-y4p0_kwlq383-i@d1$_+ig3ye&khp@1i5xvz&slt84vw)rkv))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS=['*']
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'user_profile',
 ]
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'archymedes_bk_challenge.urls'
@@ -123,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
