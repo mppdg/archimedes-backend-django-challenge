7408fe55e3e355362d386da2b609c18c307dc981
Mapopo (Evans) <mppdgreat@gmail.com>
2022-02-04 18:51:04 +0100

    Add test case for put endpoint

--- archymedes_bk_challenge/user_profile/tests/test_views.py
+++ archymedes_bk_challenge/user_profile/tests/test_views.py
@@ -15,7 +15,7 @@ class GetAllUsersTest(TestCase):
         Profile.objects.create(name='Tester Two', email="two@tester.com", role='Admin')
 
     def test_get_all_user_profiles(self):
-        response = client.get('/api/v1/users/')
+        response = client.get(reverse('get_post_users'))
         users = Profile.objects.all()
         serializer = ProfileSerializer(users, many=True)
         self.assertEqual(response.data.get('users'), serializer.data)
@@ -41,9 +41,43 @@ class CreateUserTest(TestCase):
         }
 
     def test_create_valid_user(self):
-        response = client.post('/api/v1/users/', data=json.dumps(self.valid_payload), content_type='application/json')
+        response = client.post(reverse('get_post_users'), data=json.dumps(self.valid_payload), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_invalid_puppy(self):
-        response = client.post('/api/v1/users/', data=json.dumps(self.invalid_payload), content_type='application/json')
+        response = client.post(reverse('get_post_users'), data=json.dumps(self.invalid_payload), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class UpdateSingleUserTest(TestCase):
+    """ Test module for updating an existing user profile """
+
+    def setUp(self):
+        self.tester_one = Profile.objects.create(name='Tester One', email="one@tester.com", role='User')
+        self.tester_two = Profile.objects.create(name='Tester Two', email="two@tester.com", role='Admin')
+
+        self.valid_payload = {
+            'user': {
+                'name': 'Updated Name',
+                'role': 'Admin'
+            }
+        }
+
+        self.invalid_payload = {
+            'user': {
+                'name': '',
+                'role': 'Newrole',
+            }
+        }
+
+    def test_valid_update_user(self):
+        response = client.put(reverse('update_delete_user', kwargs={'user_id': self.tester_one.pk}),
+                               data=json.dumps(self.valid_payload),
+                               content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_invalid_update_user(self):
+        response = client.put(reverse('update_delete_user', kwargs={'user_id': self.tester_two.pk}),
+                               data=json.dumps(self.invalid_payload),
+                               content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- archymedes_bk_challenge/user_profile/urls.py
+++ archymedes_bk_challenge/user_profile/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .views import ProfileView
 
-app_name = 'user_profile'
-
 urlpatterns = [
     path('users/', ProfileView.as_view(), name="get_post_users"),
     path('users/<int:user_id>', ProfileView.as_view(), name="update_delete_user")
